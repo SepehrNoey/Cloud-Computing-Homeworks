91063f8a2ad8efb12f0b67748211d6d3473cb247
SepehrNoey <sepehr.nk.81@aut.ac.ir>
2024-04-12 14:11:32 +0330

    feat: mandatory parts of phase1 completed

--- HW2/phase1/main.go
+++ HW2/phase1/main.go
@@ -4,19 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/SepehrNoey/Cloud-Computing-Homeworks/phase1/handler"
+	"github.com/SepehrNoey/Cloud-Computing-Homeworks/handler"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	rdsClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	esClient, err := elasticsearch.NewDefaultClient()
+	esClient, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{"http://elasticsearch:9200"},
+	})
 	if err != nil {
 		panic(err)
 	}
