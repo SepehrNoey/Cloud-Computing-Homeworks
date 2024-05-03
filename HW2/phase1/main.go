b41ecf5d8c0bc7ea28c43c514d6387acdf7a5326
SepehrNoey <sepehr.nk.81@aut.ac.ir>
2024-05-03 18:41:01 +0330

    feat: part 1 of HW2 phase 3 completed

--- HW2/phase1/main.go
+++ HW2/phase1/main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/SepehrNoey/Cloud-Computing-Homeworks/handler"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/redis/go-redis/v9"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
+	// Kubernetes client
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Config Map
+	cm, err := clientset.CoreV1().ConfigMaps("default").Get(context.Background(), "my-config-map", metav1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db, _ := strconv.Atoi(cm.Data["REDIS_DB"])
+
 	rdsClient := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
-		DB:       0,
+		Addr:     cm.Data["REDIS_ADDR"],
+		Password: cm.Data["REDIS_PASSWORD"],
+		DB:       db,
 	})
 
 	esClient, err := elasticsearch.NewClient(elasticsearch.Config{
-		Addresses: []string{"http://elasticsearch:9200"},
+		Addresses: []string{cm.Data["ELASTICSEARCH_ADDR"]},
 	})
 	if err != nil {
 		panic(err)
@@ -27,6 +51,6 @@ func main() {
 
 	fmt.Println("Server starting...")
 	http.HandleFunc("/", sh.SearchMovie)
-	http.ListenAndServe(":2024", nil)
+	http.ListenAndServe(cm.Data["SERVER_LISTEN_ADDR"], nil)
 
 }
